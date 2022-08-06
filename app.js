1ecb83b7b398f806b3b25b5dd4fb95cd63fa61db
panda1 <blueblue0011@gmail.com>
2022-08-05 23:09:37 -0700

    now the event listener listens to space and abc being pressed

--- app.js
+++ app.js
@@ -5,4 +5,26 @@ function move_right() {
     document.querySelector(`#dog_page`)[`style`][`transform`] = "translate(500px,0)";
 }
 
-// 
+// event listener for click on the h1 id tag and calls function when it does
+document.getElementById(`h1_page`).addEventListener(`click`, rotate_this);
+// rotates the h1_page id 100deg around the plane x
+function rotate_this() {
+    document.getElementById(`h1_page`)[`style`][`transform`] = "rotateX(100deg)";
+}
+
+document.body.addEventListener(`keydown`, pressed_something);
+
+function pressed_something() {
+    
+    if(event.key === `a`) {
+         document.body[`outerHTML`] = `<h1> You've pressed the "a" key</h1>`;
+    } else if (event.key === ` `) {
+        document.body[`outerHTML`] = `<h1> You've pressed the "space bar" key</h1>`
+    } else if (event.key === `b`) {
+        document.body[`outerHTML`] = `<h1> You've pressed the "b" key</h1>`;
+   } else if (event.key === `c`) {
+    document.body[`outerHTML`] = `<h1> You've pressed the "c" key</h1>`;
+}
+    
+   
+}
